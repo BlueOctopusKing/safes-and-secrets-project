41558065d8dcc4d49e28766bf43ee3abbd410757
BlueOctopusKing <92817520+BlueOctopusKing@users.noreply.github.com>
2021-12-07 13:30:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,18 @@
 let value = 0
-radio.onReceivedString(function (receivedString) {
-    if (true) {
-        while (true) {
-            for (let index = 0; index < 4; index++) {
-                basic.showLeds(`
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    # # # # #
-                    `)
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    . . . . .
-                    `)
-                basic.clearScreen()
-                basic.pause(500)
-            }
-            for (let index = 0; index < 4; index++) {
-                music.playTone(988, music.beat(BeatFraction.Whole))
-                music.rest(music.beat(BeatFraction.Whole))
-            }
-            basic.showString(receivedString)
-        }
-    } else {
-        for (let index = 0; index < 4; index++) {
-            basic.showString("(Safe)")
-            basic.clearScreen()
-            basic.pause(100)
-            basic.showIcon(IconNames.Yes)
-        }
-    }
-})
+radio.setGroup(80)
 basic.forever(function () {
     radio.setGroup(80)
-    if (value < 300) {
-        value = pins.analogReadPin(AnalogPin.P1)
-        while (true) {
-            radio.sendString("!Intruder!")
-            for (let index = 0; index < 5; index++) {
-                music.playTone(988, music.beat(BeatFraction.Whole))
-                music.rest(music.beat(BeatFraction.Whole))
-            }
-        }
-    } else if (value > 300) {
-        for (let index = 0; index < 4; index++) {
-            basic.showString("(Safe)")
-            basic.clearScreen()
-            basic.pause(100)
-            basic.showIcon(IconNames.Yes)
-        }
+    value = pins.analogReadPin(AnalogPin.P1)
+    basic.showNumber(value)
+    if (value < 500) {
+        radio.sendString("!Intruder!")
+        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.rest(music.beat(BeatFraction.Whole))
+    } else if (value > 500) {
+        radio.sendString("Safe")
+        basic.showString("(Safe)")
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showIcon(IconNames.Yes)
     }
 })
